ef402eed882530e2c125b4aba73dbc5357b4623e
Mohammad Javad Ahmadi <mohammad.j.ahmadi@gmail.com>
2017-02-13 02:45:43 +0330

    Add files via upload

---
+++ scripts/strip.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+/tools/bin/find /usr/lib -type f -name \*.a \
+-exec /tools/bin/strip --strip-debug {} ';'
+/tools/bin/find /lib /usr/lib -type f -name \*.so* \
+-exec /tools/bin/strip --strip-unneeded {} ';'
+/tools/bin/find /{bin,sbin} /usr/{bin,sbin,libexec} -type f \
+-exec /tools/bin/strip --strip-all {} ';'
+
