8d556945e26c00139d81b2d3e8eceb64e931557c
Mohammad Javad Ahmadi <mohammad.j.ahmadi@gmail.com>
2017-03-08 15:43:44 +0330

    Add files via upload

---
+++ scripts/configmaker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo -e "${BOLD_TXT}This program will guide you to create a xml config file for Ghoghnoos distro.${NORMAL_TXT}\n\n"
+
+echo -e "\nroot's password: "
+read -rs HOST_ROOT_PASS
