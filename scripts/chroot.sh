25975edb264d58d0286db26cb6ee8d65c59e65b5
Mohammad Javad Ahmadi <mohammad.j.ahmadi@gmail.com>
2017-03-12 14:29:18 +0330

    Add files via upload

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
 
+NORMAL="\e[39m"
+WHITE="\e[37m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+RED="\e[31m"
+
+BOLD_TXT=$(tput bold)
+NORMAL_TXT=$(tput sgr0)
+
 if [[ -s "/tools/bin/env" ]]; then
 	env_tmp=/tools/bin/env
 	path_tmp=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin
 	bash_tmp=/tools/bin/bash
-
 else
 	env_tmp=/usr/bin/env
 	path_tmp=/bin:/usr/bin:/sbin:/usr/sbin
 	bash_tmp=/bin/bash
 fi
 
-chroot "$(pwd)"         \
-$env_tmp -i             \
-HOME=/root              \
-TERM="$TERM"            \
-PS1='\u:\w\$ '          \
-PATH=$path_tmp          \
+chroot "$(pwd)"                                         \
+$env_tmp -i                                             \
+HOME=/root                                              \
+TERM="$TERM"                                            \
+PS1="$BOLD_TXT$RED\u@chroot:\w\$ $NORMAL$NORMAL_TXT"    \
+PATH=$path_tmp                                          \
 $bash_tmp --login +h
 
