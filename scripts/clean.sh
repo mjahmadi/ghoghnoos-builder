8cd8453d6e04d9753dc558c28ce23500faee33e8
Mohammad Javad Ahmadi <mohammad.j.ahmadi@gmail.com>
2017-03-20 11:31:47 +0330

    Add files via upload

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,37 +1,15 @@
 #!/bin/bash
 
 
-if [[ $UID == 0 ]]; then
-	echo -e "\nCleaning project directory tree..."
-	rm -rf ./bin
-	rm -rf ./boot
-	rm -rf ./build
-	rm -rf ./dev
-	rm -rf ./etc
-	rm -rf ./home
-	rm -rf ./iso
-	rm -rf ./lib
-	rm -rf ./lib64
-	rm -rf ./media
-	rm -rf ./mnt
-	rm -rf ./opt
-	rm -rf ./proc
-	rm -rf ./root
-	rm -rf ./run
-	rm -rf ./sbin
-	rm -rf ./srv
-	rm -rf ./sys
-	rm -rf ./tmp
-	rm -rf ./tools
-	rm -rf ./usr
-	rm -rf ./var
-	rm -rf ./init
-	echo -e "done."
+username=$USER
+group=$(id -g -n $username)
 
-	echo -e "\nChanging project directory ownership..."
-	chown -R $USER $(pwd)
-	echo -e "done.\n\n"
-else
-	echo -e "You need to be root or use sudo.\n"
-fi
+echo -e "\nCleaning project directory tree..."
+sudo rm -rf ./bin ./boot ./build ./dev ./etc ./home ./iso ./lib ./lib64 ./media ./mnt ./opt ./proc ./root ./run ./sbin \
+./srv ./sys ./tmp ./tools ./usr ./var ./init
+echo -e "done."
+
+echo -e "\nChanging project directory ownership..."
+sudo chown -R $username:$group $(pwd)
+echo -e "done.\n"
 
